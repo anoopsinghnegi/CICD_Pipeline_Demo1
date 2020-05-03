0c9cd91781e2a6bf436ba68ee93028c528c890e5
Anoop Singh Negi <negi.anoopsingh@gmail.com>
2020-05-03 14:46:59 +0000

    simple war project added

---
+++ tomcat_war/src/main/java/war/App.java
@@ -0,0 +1,13 @@
+package war;
+
+/**
+ * Dummy Application!!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Tomcat Application, Happy Learning" );
+    }
+}
